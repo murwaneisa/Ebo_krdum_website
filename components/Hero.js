aaf2e28be80434064f44f675dbf2853df67a123f
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-07-21 15:34:01 +0200

    fix positioning of Ebo image on home page

--- components/Hero.js
+++ components/Hero.js
@@ -1,29 +1,28 @@
 import { Box } from "@chakra-ui/layout";
 import Image from "next/image";
-import ButtonUi from "../UI/Button";
 
 const Hero = () => {
-	return (
-		<Box
-			m="0 auto"
-			height="auto"
-			width="auto"
-			bgColor="yellow"
-			align="center"
-			justifyItems="end"
-			pb="0"
-			position="relative"
-		>
-			<Box position="relative" pb="0">
-				<Image
-					src="/images/ebo.png"
-					objectFit="contain"
-					width={500}
-					height={500}
-				/>
-			</Box>
-		</Box>
-	);
+  return (
+    <Box
+      m="0 auto"
+      height="auto"
+      width="auto"
+      bgColor="yellow"
+      align="center"
+      justifyItems="end"
+      pb="0"
+      position="relative"
+    >
+      <Box position="relative" pb="0" bottom="-1.5">
+        <Image
+          src="/images/ebo.png"
+          objectFit="contain"
+          width={500}
+          height={500}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Hero;
