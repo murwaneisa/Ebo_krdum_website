ae1f2a2bdce4009c8bd278a277b38dd16d8eda72
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-07-25 12:14:00 +0200

    responsive button text and size and start of positioning of heading and button of Hero

--- UI/Button.js
+++ UI/Button.js
@@ -1,22 +1,24 @@
-import { Button, Text } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 const ButtonUi = (props) => {
-	return (
-		<div>
-			<Button
-				_hover={{ bg: "primary.600" }}
-				_active={{
-					bg: "primary.700",
-					transform: "scale(0.98)",
-				}}
-				colorScheme="primary"
-				variant="solid"
-				borderRadius="178"
-			>
-				{props.children}
-			</Button>
-		</div>
-	);
+  return (
+    <Box>
+      <Button
+        _hover={{ bg: "primary.600" }}
+        _active={{
+          bg: "primary.700",
+          transform: "scale(0.98)",
+        }}
+        colorScheme="primary"
+        variant="solid"
+        borderRadius="178"
+        fontSize={["sm", "md", "lg"]}
+        px={["10px", "14px", "18px"]}
+      >
+        {props.children}
+      </Button>
+    </Box>
+  );
 };
 
 export default ButtonUi;
--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,6 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Text } from "@chakra-ui/layout";
 import Image from "next/image";
+import Button from "../UI/Button";
 
 const Hero = () => {
   return (
@@ -14,12 +15,20 @@ const Hero = () => {
       position="relative"
     >
       <Box position="relative" pb="0" bottom="-1.5">
+        <Box position="absolute" right="4" top="5">
+          <Text>Orre Tora Tasds</Text>
+        </Box>
         <Image
           src="/images/ebo.png"
           objectFit="contain"
           width={500}
           height={500}
+          border="2px solid red"
         />
+
+        <Box>
+          <Button>View Album</Button>
+        </Box>
       </Box>
     </Box>
   );
