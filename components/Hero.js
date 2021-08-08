4d4728baeac52c4fbee66b46f7d426e4465ba281
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-08-08 12:35:33 +0200

    add albums data

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,8 @@
-import { Box, Text, Flex } from "@chakra-ui/layout";
-/* Component */
-import Image from "../ui/Image";
+import { Box, Flex, Text } from "@chakra-ui/layout";
 //import Section from "../UI/Section";
 import Button from "../UI/Button";
+/* Component */
+import Image from "../ui/Image";
 /*
  * the array of percentage({["10%", "40%"]}) represent the  breakpoint for the element start from mobile to lager screen
  * check the link for more details:
@@ -10,45 +10,46 @@ import Button from "../UI/Button";
  */
 
 const Hero = () => {
-	return (
-		<Box
-			w="100%"
-			h="auto"
-			pt={["4.6rem", "6.2rem"]}
-			pb={["1.2rem", "0rem"]}
-			bg="linear-gradient(180deg, #C2B552 0%, #795806 100%)"
-			align="center"
-		>
-			<Flex direction={["column", "row"]} w={["100%", "96%", "90%", "80%"]}>
-				<Image
-					src="/images/ebo-transparent.png"
-					layout="fill"
-					width="100%"
-					objectFit="contain"
-				/>
-				<Flex
-					direction="column"
-					justify={["flex-start", "center"]}
-					align="center"
-				>
-					<Box>
-						<Text
-							color="brown"
-							fontWeight="bold"
-							fontStyle="italic"
-							fontSize={["3.8rem", "4.4rem", "6.4rem", "7rem", "8.4rem"]}
-							fontFamily="Brush Script MT"
-						>
-							Diversity
-						</Text>
-					</Box>
-					<Box>
-						<Button>View latest Album</Button>
-					</Box>
-				</Flex>
-			</Flex>
-		</Box>
-	);
+  return (
+    <Box
+      w="100%"
+      h="auto"
+      pt={["4.6rem", "6.2rem"]}
+      pb={["1.2rem", "0rem"]}
+      bg="linear-gradient(180deg, #C2B552 0%, #795806 100%)"
+      align="center"
+    >
+      <Flex direction={["column", "row"]} w={["100%", "96%", "90%", "80%"]}>
+        <Image
+          src="/images/ebo-transparent.png"
+          alt="This is the main hero image of Ebo Krdums website"
+          layout="fill"
+          width="100%"
+          objectFit="contain"
+        />
+        <Flex
+          direction="column"
+          justify={["flex-start", "center"]}
+          align="center"
+        >
+          <Box>
+            <Text
+              color="brown"
+              fontWeight="bold"
+              fontStyle="italic"
+              fontSize={["3.8rem", "4.4rem", "6.4rem", "7rem", "8.4rem"]}
+              fontFamily="Brush Script MT"
+            >
+              Diversity
+            </Text>
+          </Box>
+          <Box>
+            <Button>View latest Album</Button>
+          </Box>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Hero;
--- public/locale/en/albums.js
+++ public/locale/en/albums.js
@@ -1,9 +1,9 @@
 export default [
   {
-    id: "album1",
+    id: "anasna",
     title: "Anasna",
     description:
-      "Album Description/Story: This was the official release for Ebo Krdum. Although it was a trial-release as a further step to reach his fans outside Sweden. Three singles he wrote with two different languages (Daju, Sudan-Arabi) . Recorded in Stockholm by his co-player John Runefelt. These releases were from the project : Diary of War. As Ebo describes it (songs that he wrote during the war in Darfur-Sudan) ",
+      "This was the official release for Ebo Krdum. Although it was a trial-release as a further step to reach his fans outside Sweden. Three singles he wrote with two different languages (Daju, Sudan-Arabi) . Recorded in Stockholm by his co-player John Runefelt. These releases were from the project : Memory of War. As Ebo describes it 'songs that he wrote during the war in Darfur-Sudan'. All music & Lyrics by : Ebo Krdum.",
     year: "2018",
     image: "/anasna.jpg",
     photographer: "Victoria Moralez",
@@ -11,82 +11,157 @@ export default [
       {
         name: "Anasna",
         time: "5:58",
-        spotify: "",
+        spotify:
+          "https://open.spotify.com/track/26p7UHX8SZtTzMA0BUYSEh?si=b70066f5da634eaf",
+        trialSong: "/songs/anasna/1-anasna.mp3",
       },
       {
         name: "Bong Farang",
         time: "7:09",
-        spotify: "",
+        spotify:
+          "https://open.spotify.com/track/6v5p7AO2GaFJ2WHbWlcR72?si=a9bacab3679244fd",
+        trialSong: "/songs/anasna/2-bong-farang.mp3",
       },
       {
         name: "Afe Kamounga",
         time: "6:23",
-        spotify: "",
+        spotify:
+          "https://open.spotify.com/track/3OkqyRb1znNDskBtCzy7fA?si=0a9428054a894a5a",
+        trialSong: "/songs/anasna/3-afe-kamounga.mp3",
       },
     ],
   },
   {
-    id: "album2",
+    id: "salam",
     title: "Salam",
-    description: "this Salam description",
+    description:
+      "The album Salam contains 5 songs that were released at the same time (2019) and one more song ‘Devastation’ which was released via the label company Supertraditional in 2020. But all the 6 songs were recorded as part of the same project (Memory of War). It was recorded in Stockholm, by Ebo’s co-player & producer John Runefelt. They were written in (Sudan-Arabi, Juba-Arabi & Daju) with some mix of Swedish, French & English, in some songs. All music & Lyrics by : Ebo Krdum.",
     year: "2019",
     image: "/salam.jpg",
-    photographer: "Simon Sahara",
+    photographer: "Simon Sahara & cover-design by : Khalid Faroq",
     songs: [
       {
         name: "Home for All",
         time: "4:46",
         spotify:
           "https://open.spotify.com/track/2LHzMrUPwp9ocwq0evuJbE?si=206030c703b641ba",
-      },
-      {
-        name: "Back in the Days",
-        time: "8:07",
-        spotify:
-          "https://open.spotify.com/track/07j0RpRs9JgPaTDnqTEh1A?si=1a60585585424c67",
+        trialSong: "/songs/salam/1-home-for-all.mp3",
       },
       {
         name: "Cry Alone",
         time: "3:57",
         spotify:
           "https://open.spotify.com/track/0q30UUMbTW96FSxiD7CpQY?si=103feef1fb4547a4",
+        trialSong: "/songs/salam/2-cry-alone.mp3",
       },
       {
         name: "Peace for IDPs",
         time: "5:22",
         spotify:
           "https://open.spotify.com/track/71hm5pfZ1AP8X6rPlPAYxa?si=b166188640634568",
+        trialSong: "/songs/salam/3-peace-for-IDPs.mp3",
       },
+      {
+        name: "Back in the Days",
+        time: "8:07",
+        spotify:
+          "https://open.spotify.com/track/07j0RpRs9JgPaTDnqTEh1A?si=1a60585585424c67",
+        trialSong: "/songs/salam/4-back-in-the-days.mp3",
+      },
+
       {
         name: "Akheeda",
         time: "6:34",
         spotify:
           "https://open.spotify.com/track/0p5531cLYHHGK89P8AD6z1?si=c69bc2ebc0524b88",
+        trialSong: "/songs/salam/5-akheeda.mp3",
+      },
+      {
+        name: "Devastation",
+        time: "5:25",
+        spotify:
+          "https://open.spotify.com/track/3Syc59WbdgtsR5ZOrA2DzC?si=ad400efffaac4657",
+        trialSong: "/songs/salam/6-devastation.mp3",
       },
     ],
   },
-  /* 	{
-		id: "album3",
-		title: "Diversity",
-		description: "this diversity description",
-		year: "2021",
-		image: "/diversity.jpg",
-		songs: [
-			{
-				name: "Anasna",
-				time: "5:58",
-				spotify: "",
-			},
-			{
-				name: "Bong Farang",
-				time: "7:09",
-				spotify: "",
-			},
-			{
-				name: "Afe Kamounga",
-				time: "6:23",
-				spotify: "",
-			},
-		],
-	}, */
+  {
+    id: "diversity",
+    title: "Diversity",
+    description:
+      "'Diversity' is not just a musical and cultural melting pot, the lyrics that tell both Ebo´s life story and political topics such as equality, diversity and freedom, are sung in no less than eight different languages. [Fulani, Beri (Zaghawa), Fur, Sudan Arabic, Massara (Masaleet), Beja, Daju and English]. The album Diversity is from Ebo’s project named : The Sub-Saharan Jigs.",
+    year: "2021",
+    image: "/diversity.jpg",
+    songs: [
+      {
+        name: "Worbé é Rewbé",
+        time: "4:54",
+        spotify: "",
+        trialSong: "/songs/diversity/1-worbe-e-rewbe",
+      },
+      {
+        name: "Revolution Call",
+        time: "4:47",
+        spotify:
+          "https://open.spotify.com/track/4oo2hn59gOeGpSoTPV60IE?si=b58b68208d4f4ae1",
+        trialSong: "/songs/diversity/2-revolution-call",
+      },
+      {
+        name: "Dabaywah",
+        time: "4:31",
+        spotify: "",
+        trialSong: "/songs/diversity/3-dabaywah",
+      },
+      {
+        name: "Almanfa",
+        time: "5:29",
+        spotify: "",
+        trialSong: "/songs/diversity/4-almanfa",
+      },
+      {
+        name: "ôrré tog'íŋ dogólá",
+        time: "3:39",
+        spotify:
+          "https://open.spotify.com/track/0AlJJnP1NYsLZ2rwkvn2VH?si=0c1b172c1b8d4371",
+        trialSong: "/songs/diversity/5-orre-tog-in-dogola",
+      },
+
+      {
+        name: "Genyah",
+        time: "7:20",
+        spotify: "",
+        trialSong: "/songs/diversity/6-genyah",
+      },
+      {
+        name: "Selikai",
+        time: "4:02",
+        spotify: "",
+        trialSong: "/songs/diversity/7-selikai",
+      },
+      {
+        name: "Thaori",
+        time: "5:06",
+        spotify: "",
+        trialSong: "/songs/diversity/8-thaori",
+      },
+      {
+        name: "Gânê Feretaye",
+        time: "5:29",
+        spotify: "",
+        trialSong: "/songs/diversity/9-gane-feretaye",
+      },
+      {
+        name: "Aysé Konga",
+        time: "4:15",
+        spotify: "",
+        trialSong: "/songs/diversity/10-ayse-konga",
+      },
+      {
+        name: "Diar Hanana",
+        time: "3:37",
+        spotify: "",
+        trialSong: "/songs/diversity/11-diar-hanana",
+      },
+    ],
+  },
 ];
