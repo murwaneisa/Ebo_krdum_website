3069bc7f020efbb6ad3a111cf356c349e815e5f6
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2021-08-09 18:22:33 +0200

    add the shows data to locate file and make some toddos

--- public/locale/en/shows.js
+++ public/locale/en/shows.js
@@ -1,14 +1,87 @@
-export default [
+export const uppComingShows = [
 	{
-		id: "1",
-		date: " Saturday 2021-10-15",
-		time: "18:00",
-		location: "Fasching, Kungsgatan 13, Stockholm",
+		id: "c1",
+		sponsor: " Fasching",
+		date: "2021-09-30",
+		time: "20:00",
+		location: "Kungsgatan 63 , Stockholm",
+		link: " https://www.fasching.se/ebo-krdum/#2021-09-30T20-00",
 	},
 	{
-		id: "2",
-		date: "sunday 2022-4-15",
-		time: "15:00",
-		location: "Kontorgatan, Kungsgatan 13, Uppsala",
+		id: "c2",
+		sponsor: "Folkteatern Göteborg",
+		date: "2021-10-09",
+		time: "21:00",
+		location: " Folkteatern Göteborg, Olof Palmes Plats, Gothenburg",
+		link: "https://stayhappening.com/e/konsert-ebo-krdum-E2ISTRVLB16",
+	},
+	{
+		id: "c3",
+		sponsor: "Showcase at Womex",
+		date: "2021-10-31",
+		time: "21:00",
+		location: " Porto-Portugal",
+		link: "https://www.womex.com/programme/showcases#artists",
+	},
+];
+
+export const formerShows = [
+	{
+		id: "f1",
+		sponsor: "Ställbergs Gruvan",
+		date: "2021-07-15",
+		location: " Ställbergs Gruvan , Kopparberg-Sweden",
+		link: "https://b-m.facebook.com/stallbergs.gruva/photos/a.682793688464344/4024386317638381/?type=3&source=48",
+	},
+	{
+		id: "f2",
+		sponsor: "Sommarscen Malmö",
+		date: "2021-7-03",
+		location: " Borggården, Malmö hus slott-Sweden",
+		link: "https://b-m.facebook.com/stallbergs.gruva/photos/a.682793688464344/4024386317638381/?type=3&source=48",
+	},
+	{
+		id: "f3",
+		sponsor: "Stallet",
+		date: "2021-05-07",
+		location: "Stallgatan 7 - Stockholm-Sweden",
+		link: "https://www.stallet.st/sv/7081-ebo-krdum-genuine-mezziga-210507",
+	},
+	{
+		id: "f4",
+		sponsor: " Selam",
+		date: "2021-04-21",
+		location: "Live stream in Stockholm-Sweden",
+		link: "http://selam.se/events/selam-sessions-ebo-krdum/",
+	},
+	{
+		id: "f5",
+		sponsor: " Clandestion Festival",
+		date: "2020-06-07",
+		time: "20:00",
+		location: "Hagakyrkan, Gothenburg",
+		link: "http://clandestinofestival.org/2020/en/ebo-krdum/",
+	},
+	{
+		id: "f6",
+		sponsor: "Genuine Mezziga ",
+		date: "2019-09-07",
+		location: "Gothenburg-Sweden",
+		link: "https://www.musikenshus.se/?s=genuine+mezziga",
+	},
+	{
+		id: "f7",
+		sponsor: "Genuine Mezziga ",
+		date: "2019-07-18",
+		location: "Landsvägen 476, Skattungbyn-Sweden",
+		link: "https://www.facebook.com/events/344334906177877",
+	},
+
+	{
+		id: "f8",
+		sponsor: "world music showcase at the Edge",
+		date: "2019-04-13",
+		location: "Birmingham, united kingdom",
+		link: "https://www.facebook.com/events/the-edge/ebo-krdum-hassan-salih-nour/1107746976096800/",
 	},
 ];
