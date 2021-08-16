ee2b523ed76393333912394093193cb10516cdf7
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2021-08-16 08:58:10 +0200

    add alt to image and fix the map function key erro

--- components/UI/CustomImage.js
+++ components/UI/CustomImage.js
@@ -2,15 +2,15 @@ import Image from "next/image";
 import Styles from "../../styles/image.module.css";
 
 const CustomImage = ({ height, maxHeight, width, maxWidth, ...rest }) => {
-  let widths = {};
-  width ? (widths["width"] = width) : "100%";
-  maxWidth ? (widths["maxWidth"] = maxWidth) : "100%";
+	let widths = {};
+	width ? (widths["width"] = width) : "100%";
+	maxWidth ? (widths["maxWidth"] = maxWidth) : "100%";
 
-  return (
-    <div className={Styles.imageContainer} style={widths}>
-      <Image className={Styles.image} {...rest} />
-    </div>
-  );
+	return (
+		<div className={Styles.imageContainer} style={widths}>
+			<Image className={Styles.image} {...rest} alt="this the custom image " />
+		</div>
+	);
 };
 
 export default CustomImage;
