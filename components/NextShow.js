aa4982e8348c2363786c6c0ad47df985c25d1717
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2021-08-17 18:03:51 +0200

    change the next show card color

--- components/NextShow.js
+++ components/NextShow.js
@@ -1,73 +1,72 @@
 import {
-  Box,
-  Flex,
-  Heading,
-  Link as ChakraLink,
-  Stack,
+	Box,
+	Flex,
+	Heading,
+	Link as ChakraLink,
+	Stack,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { BiCalendarCheck, BiLocationPlus, BiTimeFive } from "react-icons/bi";
 import ButtonUi from "./UI/Button";
 
 const NextShow = () => {
-  return (
-    <Box w="100vw" align="center" justify="center">
-      <Stack
-        bg="yellow"
-        p={["10%", "5%"]}
-        mx={["5%", "10%", "20%"]}
-        mb={["2rem", "6rem"]}
-        spacing="20px"
-      >
-        <Heading as="h3" size="xl" pb="1rem">
-          FASCHING
-        </Heading>
-        <Stack
-          justify="center"
-          spacing={["2%", "10%", "15%"]}
-          direction={["column", "row"]}
-        >
-          <Flex
-            fontSize={["1rem", "1.2rem"]}
-            align="center"
-            justify={["center", "center"]}
-          >
-            <BiCalendarCheck />
-            <Box pl="0.4rem">2021-SPE-30</Box>
-          </Flex>
+	return (
+		<Box w="100vw" align="center" justify="center">
+			<Stack
+				bg="yellow"
+				p={["10%", "5%"]}
+				mx={["5%", "10%", "20%"]}
+				mb={["2rem", "6rem"]}
+				spacing="20px"
+			>
+				<Heading as="h3" size="xl" pb="1rem">
+					FASCHING
+				</Heading>
+				<Stack
+					justify="center"
+					spacing={["2%", "10%", "15%"]}
+					direction={["column", "row"]}
+				>
+					<Flex
+						fontSize={["1rem", "1.2rem"]}
+						align="center"
+						justify={["center", "center"]}
+					>
+						<BiCalendarCheck />
+						<Box pl="0.4rem">2021-SPE-30</Box>
+					</Flex>
 
-          <Flex fontSize={["1rem", "1.2rem"]} align="center" justify="center">
-            <BiTimeFive />
-            <Box pl="0.4rem">20:00</Box>
-          </Flex>
-        </Stack>
-        <Flex
-          fontSize={["1rem", "1.2rem"]}
-          align="center"
-          justify={["center", "center"]}
-        >
-          <BiLocationPlus />
+					<Flex fontSize={["1rem", "1.2rem"]} align="center" justify="center">
+						<BiTimeFive />
+						<Box pl="0.4rem">20:00</Box>
+					</Flex>
+				</Stack>
+				<Flex
+					fontSize={["1rem", "1.2rem"]}
+					align="center"
+					justify={["center", "center"]}
+				>
+					<BiLocationPlus />
 
-          <Box pl={["0rem", "0.4rem", "0.4rem"]}>Kungsgatan 63 , Stockholm</Box>
-        </Flex>
-        <ChakraLink
-          href="https://www.fasching.se/ebo-krdum/#2021-09-30T20-00"
-          isExternal
-        >
-          <ButtonUi>Book tickets</ButtonUi>
-        </ChakraLink>
-        <Box justify="center" align="center">
-          <Link
-            /* change the link */
-            href="/shows"
-            color="brown"
-          >
-            <a>See all shows</a>
-          </Link>
-        </Box>
-      </Stack>
-    </Box>
-  );
+					<Box pl={["0rem", "0.4rem", "0.4rem"]}>Kungsgatan 63 , Stockholm</Box>
+				</Flex>
+				<ChakraLink
+					href="https://www.fasching.se/ebo-krdum/#2021-09-30T20-00"
+					isExternal
+				>
+					<ButtonUi>Book tickets</ButtonUi>
+				</ChakraLink>
+				<Box justify="center" color="brown" align="center">
+					<Link
+						/* change the link */
+						href="/shows"
+					>
+						<a>See all shows</a>
+					</Link>
+				</Box>
+			</Stack>
+		</Box>
+	);
 };
 
 export default NextShow;
