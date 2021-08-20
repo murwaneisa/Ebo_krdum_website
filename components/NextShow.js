96663e12b992099b2686d4d2cd56ba25d01e3576
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2021-08-20 15:46:35 +0200

    add new style for next show card

--- components/NextShow.js
+++ components/NextShow.js
@@ -8,62 +8,65 @@ import {
 import Link from "next/link";
 import { BiCalendarCheck, BiLocationPlus, BiTimeFive } from "react-icons/bi";
 import ButtonUi from "./UI/Button";
+import Image from "next/image";
 
 const NextShow = () => {
 	return (
-		<Box w="100vw" align="center" justify="center">
+		<Box w="100vw" align="center">
 			<Stack
+				direction={["column", "column", "row"]}
 				bg="yellow"
-				p={["10%", "5%"]}
+				p={["1%", "1%", "5%"]}
 				mx={["5%", "10%", "20%"]}
 				mb={["2rem", "6rem"]}
-				spacing="20px"
+				//spacing="20px"
+				//border="1px solid red"
 			>
-				<Heading as="h3" size="xl" pb="1rem">
-					FASCHING
-				</Heading>
-				<Stack
-					justify="center"
-					spacing={["2%", "10%", "15%"]}
-					direction={["column", "row"]}
-				>
-					<Flex
-						fontSize={["1rem", "1.2rem"]}
-						align="center"
-						justify={["center", "center"]}
-					>
-						<BiCalendarCheck />
-						<Box pl="0.4rem">2021-SPE-30</Box>
-					</Flex>
-
-					<Flex fontSize={["1rem", "1.2rem"]} align="center" justify="center">
-						<BiTimeFive />
-						<Box pl="0.4rem">20:00</Box>
-					</Flex>
-				</Stack>
-				<Flex
-					fontSize={["1rem", "1.2rem"]}
-					align="center"
-					justify={["center", "center"]}
-				>
-					<BiLocationPlus />
-
-					<Box pl={["0rem", "0.4rem", "0.4rem"]}>Kungsgatan 63 , Stockholm</Box>
-				</Flex>
-				<ChakraLink
-					href="https://www.fasching.se/ebo-krdum/#2021-09-30T20-00"
-					isExternal
-				>
-					<ButtonUi>Book tickets</ButtonUi>
-				</ChakraLink>
-				<Box justify="center" color="brown" align="center">
-					<Link
-						/* change the link */
-						href="/shows"
-					>
-						<a>See all shows</a>
-					</Link>
+				<Box>
+					<Image
+						src="/images/salam.jpg"
+						objectFit="contain"
+						width={500}
+						height={500}
+					/>
 				</Box>
+				<Stack
+					alignItems="center"
+					spacing={["2%"]}
+					direction="column"
+					border="1px solid blue"
+				>
+					<Heading as="h3" size="xl" pb="1rem">
+						FASCHING
+					</Heading>
+					<Flex direction="column" justify="center">
+						<Flex fontSize={["1rem", "1.2rem"]} align="center">
+							<BiCalendarCheck />
+							<Box pl="0.4rem">2021-SPE-30</Box>
+						</Flex>
+
+						<Flex fontSize={["1rem", "1.2rem"]} align="center">
+							<BiTimeFive />
+							<Box pl="0.4rem">20:00</Box>
+						</Flex>
+						<Flex fontSize={["1rem", "1.2rem"]} align="center">
+							<BiLocationPlus />
+							<Box pl="0.4rem">Kungsgatan 63, Stockholm</Box>
+						</Flex>
+					</Flex>
+
+					<ChakraLink
+						href="https://www.fasching.se/ebo-krdum/#2021-09-30T20-00"
+						isExternal
+					>
+						<ButtonUi>Book tickets</ButtonUi>
+					</ChakraLink>
+					<Box color="brown" align="center">
+						<Link href="/shows">
+							<a>See all shows</a>
+						</Link>
+					</Box>
+				</Stack>
 			</Stack>
 		</Box>
 	);
--- components/UI/Button.js
+++ components/UI/Button.js
@@ -6,26 +6,26 @@ import { Box, Button } from "@chakra-ui/react";
  */
 
 const ButtonUi = ({ marginRight, children, ...rest }) => {
-  return (
-    <Box>
-      <Button
-        _hover={{ bg: "primary.600" }}
-        _active={{
-          bg: "primary.700",
-          transform: "scale(0.98)",
-        }}
-        colorScheme="primary"
-        variant="solid"
-        borderRadius="178"
-        fontSize={["xs", "sm", "md", "md"]}
-        px={["0.8rem", "0.7rem", "0.8rem", "1.2rem"]}
-        py={["0.4rem", "0.7rem", "0.8rem", "1.2rem"]}
-        mr={marginRight}
-      >
-        {children}
-      </Button>
-    </Box>
-  );
+	return (
+		<Box>
+			<Button
+				_hover={{ bg: "primary.600" }}
+				_active={{
+					bg: "primary.700",
+					transform: "scale(0.98)",
+				}}
+				colorScheme="primary"
+				variant="solid"
+				borderRadius="178"
+				fontSize={["xs", "sm", "md", "md"]}
+				px={["0.8rem", "0.7rem", "0.8rem", "1.2rem"]}
+				py={["0.4rem", "0.7rem", "0.8rem", "1.2rem"]}
+				mr={marginRight}
+			>
+				{children}
+			</Button>
+		</Box>
+	);
 };
 
 export default ButtonUi;
