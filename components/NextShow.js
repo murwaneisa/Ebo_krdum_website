827dd9b86571392dc6fe0cae1673c896db3edaff
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2021-09-05 12:30:36 +0200

    fix the error handlig for album request  and add new image for the next show component

--- components/NextShow.js
+++ components/NextShow.js
@@ -1,9 +1,9 @@
 import {
-  Box,
-  Flex,
-  Heading,
-  Link as ChakraLink,
-  Stack,
+	Box,
+	Flex,
+	Heading,
+	Link as ChakraLink,
+	Stack,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,91 +12,91 @@ import classes from "../styles/roundImage.module.css";
 import ButtonUi from "./UI/Button";
 
 const NextShow = () => {
-  return (
-    <Flex
-      direction={["column", "column", "row"]}
-      bg="yellow"
-      w={[
-        "90vw",
-        "80vw",
-        "80vw",
-        "3xl",
-        "3xl",
-      ]} /* fix layout foe the next show and video and reviews  */
-      px={["1rem", "1rem", "1rem", "1rem", "2rem"]}
-      py={["2rem", "2rem"]}
-      align={["center", "center", "center"]}
-      justify="space-evenly"
-    >
-      {/*image section */}
-      <Box
-        align="center"
-        // border="1px white solid"
-        className={classes.image}
-      >
-        <Image
-          src="/images/ebo-next-show2.jpg"
-          alt="Event image"
-          width={400}
-          height={400}
-          objectFit="cover"
-          // layout="fill"
-        />
-      </Box>
-      {/* End of the section */}
+	return (
+		<Flex
+			direction={["column", "column", "row"]}
+			bg="yellow"
+			w={[
+				"90vw",
+				"80vw",
+				"80vw",
+				"3xl",
+				"3xl",
+			]} /* fix layout foe the next show and video and reviews  */
+			px={["1rem", "1rem", "1rem", "1rem", "2rem"]}
+			py={["2rem", "2rem"]}
+			align={["center", "center", "center"]}
+			justify="space-evenly"
+		>
+			{/*image section */}
+			<Box
+				align="center"
+				// border="1px white solid"
+				className={classes.image}
+			>
+				<Image
+					src="/images/ebo-next-show.jpg"
+					alt="Event image"
+					width={400}
+					height={400}
+					objectFit="cover"
+					// layout="fill"
+				/>
+			</Box>
+			{/* End of the section */}
 
-      {/* info section */}
-      <Stack
-        alignItems="center"
-        spacing={["2%"]}
-        direction="column"
-        ml={["0%", "0%", "5%"]}
-        mt={["2%", "2%", "0%"]}
-        //border="1px solid blue"
-        // w="80%"
-        align="center"
-      >
-        <Heading as="h3" size="xl" pb="1rem">
-          FASCHING
-        </Heading>
-        {/* End of the section */}
-        {/* date,time place section */}
-        <Stack direction="column" pb="1rem" justify="center">
-          <Flex fontSize={["1rem", "1.2rem"]} align="center">
-            <BiCalendarCheck />
-            <Box pl="0.4rem">2021-09-30</Box>
-          </Flex>
+			{/* info section */}
+			<Stack
+				alignItems="center"
+				spacing={["2%"]}
+				direction="column"
+				ml={["0%", "0%", "5%"]}
+				mt={["2%", "2%", "0%"]}
+				//border="1px solid blue"
+				// w="80%"
+				align="center"
+			>
+				<Heading as="h3" size="xl" pb="1rem">
+					FASCHING
+				</Heading>
+				{/* End of the section */}
+				{/* date,time place section */}
+				<Stack direction="column" pb="1rem" justify="center">
+					<Flex fontSize={["1rem", "1.2rem"]} align="center">
+						<BiCalendarCheck />
+						<Box pl="0.4rem">2021-09-30</Box>
+					</Flex>
 
-          <Flex fontSize={["1rem", "1.2rem"]} align="center">
-            <BiTimeFive />
-            <Box pl="0.4rem">20:00</Box>
-          </Flex>
-          <Flex fontSize={["1rem", "1.2rem"]} align="center">
-            <BiLocationPlus />
-            <Box pl="0.4rem">Kungsgatan 34, Stockholm</Box>
-          </Flex>
-        </Stack>
-        {/* End of the section */}
-        {/* button and link section */}
-        <Stack direction={["column", "row"]}>
-          <Box align="center">
-            <ChakraLink
-              href="https://www.fasching.se/ebo-krdum/#2021-09-30T20-00"
-              isExternal
-            >
-              <ButtonUi>Book tickets</ButtonUi>
-            </ChakraLink>
-          </Box>
-          <Flex color="brown" justify="center" align="center">
-            <Link href="/shows">
-              <a>See all shows</a>
-            </Link>
-          </Flex>
-        </Stack>
-        {/* End of the section */}
-      </Stack>
-    </Flex>
-  );
+					<Flex fontSize={["1rem", "1.2rem"]} align="center">
+						<BiTimeFive />
+						<Box pl="0.4rem">20:00</Box>
+					</Flex>
+					<Flex fontSize={["1rem", "1.2rem"]} align="center">
+						<BiLocationPlus />
+						<Box pl="0.4rem">Kungsgatan 34, Stockholm</Box>
+					</Flex>
+				</Stack>
+				{/* End of the section */}
+				{/* button and link section */}
+				<Stack direction={["column", "row"]}>
+					<Box align="center">
+						<ChakraLink
+							href="https://www.fasching.se/ebo-krdum/#2021-09-30T20-00"
+							isExternal
+						>
+							<ButtonUi>Book tickets</ButtonUi>
+						</ChakraLink>
+					</Box>
+					<Flex color="brown" justify="center" align="center">
+						<Link href="/shows">
+							<a>See all shows</a>
+						</Link>
+					</Flex>
+				</Stack>
+				{/* End of the section */}
+			</Stack>
+		</Flex>
+	);
 };
 
 export default NextShow;
--- pages/album/[albumId].js
+++ pages/album/[albumId].js
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import AlbumInfoSection from "../../components/album/AlbumInfoSection";
 import SongsListSection from "../../components/album/SongsListSection";
@@ -6,36 +6,42 @@ import Section from "../../components/UI/Section";
 import data from "../../public/locale/en/albums";
 
 const AlbumItems = () => {
-  // useRouter from Next
-  const router = useRouter();
-  const albumQuery = router.query.albumId;
+	// useRouter from Next
+	const router = useRouter();
+	const albumQuery = router.query.albumId;
 
-  // get album by id
-  const album = data.find((item) => item.id === albumQuery);
-  if (!album) {
-    return <Section bg="brown">Loading...</Section>;
-  }
+	// get album by id
+	const album = data.find((item) => item.id === albumQuery);
+	if (!album) {
+		return (
+			<Section bg="brown">
+				<Box pb="100%" pt="100%" justify="Center" align="center">
+					<Text>No Album with the Requested URL</Text>
+				</Box>
+			</Section>
+		);
+	}
 
-  //const { title, description, year, image, photographer, albumSpotify } = album;
+	//const { title, description, year, image, photographer, albumSpotify } = album;
 
-  return (
-    <Box>
-      {/* album info section */}
-      {/* Want to pass in data via props: album title, description, year, spotify link for all album */}
-      <AlbumInfoSection
-        title={album.title}
-        description={album.description}
-        year={album.year}
-        image={album.image}
-        photographer={album.photographer}
-        albumSpotify={album.albumSpotify}
-      />
+	return (
+		<Box>
+			{/* album info section */}
+			{/* Want to pass in data via props: album title, description, year, spotify link for all album */}
+			<AlbumInfoSection
+				title={album.title}
+				description={album.description}
+				year={album.year}
+				image={album.image}
+				photographer={album.photographer}
+				albumSpotify={album.albumSpotify}
+			/>
 
-      {/* Album Songs section */}
-      {/* Pass through album songs as props */}
-      <SongsListSection songs={album.songs} />
-    </Box>
-  );
+			{/* Album Songs section */}
+			{/* Pass through album songs as props */}
+			<SongsListSection songs={album.songs} />
+		</Box>
+	);
 };
 
 export default AlbumItems;
