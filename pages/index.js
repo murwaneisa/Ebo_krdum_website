5614bf52f0c89220c1095c91368f1afa4187e4fb
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-09-05 16:10:30 +0200

    remove link preconnect on Home Head

--- pages/index.js
+++ pages/index.js
@@ -12,60 +12,60 @@ import Video from "../components/Video";
 import data from "../public/locale/en/albums.js";
 
 export default function Home(props) {
-	return (
-		<div>
-			<Head>
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap"
-					rel="stylesheet"
-				/>
-			</Head>
-			{/* Hero image */}
-			<Hero />
-			{/* Other albums */}
-			<Section
-				bg="brown"
-				px={["2rem", "2rem", "3rem", "6.2rem"]}
-				py="2rem"
-				title="Albums"
-			>
-				<Box>
-					<Stack
-						direction={["column", "column", "row"]}
-						spacing={["5%", "1%", "1%", "3%"]}
-						align="center"
-					>
-						{data.map((album) => (
-							<AlbumCard
-								key={album.id}
-								id={album.id}
-								year={album.year}
-								image={album.image}
-								title={album.title}
-							/>
-						))}
-					</Stack>
-				</Box>
-			</Section>
+  return (
+    <div>
+      <Head>
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      {/* Hero image */}
+      <Hero />
+      {/* Other albums */}
+      <Section
+        bg="brown"
+        px={["2rem", "2rem", "3rem", "6.2rem"]}
+        py="2rem"
+        title="Albums"
+      >
+        <Box>
+          <Stack
+            direction={["column", "column", "row"]}
+            spacing={["5%", "1%", "1%", "3%"]}
+            align="center"
+          >
+            {data.map((album) => (
+              <AlbumCard
+                key={album.id}
+                id={album.id}
+                year={album.year}
+                image={album.image}
+                title={album.title}
+              />
+            ))}
+          </Stack>
+        </Box>
+      </Section>
 
-			{/* Video */}
-			<Section bg="yellow" title="Videos" py="2rem">
-				<Video />
-			</Section>
+      {/* Video */}
+      <Section bg="yellow" title="Videos" py="2rem">
+        <Video />
+      </Section>
 
-			{/*  Next Show */}
-			<Section bg="brown" title="Next Show" py="2rem">
-				<NextShow />
-			</Section>
+      {/*  Next Show */}
+      <Section bg="brown" title="Next Show" py="2rem">
+        <NextShow />
+      </Section>
 
-			{/* Review */}
-			<Section bg="yellow" pt="2rem" pb="5rem" title="Reviews">
-				<Box align="center">
-					<Reviews />
-				</Box>
-			</Section>
-		</div>
-	);
+      {/* Review */}
+      <Section bg="yellow" pt="2rem" pb="5rem" title="Reviews">
+        <Box align="center">
+          <Reviews />
+        </Box>
+      </Section>
+    </div>
+  );
 }
