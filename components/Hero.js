3dd498bcb9f85ebbb955f40eb7cec035a77b2b40
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2021-09-05 13:09:35 +0200

    adding google font to hero title

--- components/Hero.js
+++ components/Hero.js
@@ -12,58 +12,58 @@ import CustomImage from "./UI/CustomImage";
  */
 
 const Hero = () => {
-  return (
-    <Box
-      w="100vw"
-      h="auto"
-      pt={["4.6rem", "6.2rem"]}
-      pb={["1.2rem", "0rem"]}
-      bg="linear-gradient(180deg, #C2B552 0%, #795806 100%)"
-      align="center"
-    >
-      <Flex direction={["column", "row"]} w={["100%", "96%", "90%", "80%"]}>
-        <CustomImage
-          src="/images/ebo-transparent.png"
-          alt="This is the main hero image of Ebo Krdums website"
-          layout="fill"
-          width="100%"
-          objectFit="contain"
-        />
-        <VStack
-          //direction="column"
-          justify={["flex-start", "flex-end"]}
-          spacing={-1}
-          align="center"
-          my="5%"
-          //border="1px blue solid"
-        >
-          <Box>
-            <Text color="brown" fontWeight="bold" fontStyle="italic">
-              Latest Album
-            </Text>
-          </Box>
-          <Box>
-            <Text
-              color="brown"
-              fontWeight="bold"
-              fontStyle="italic"
-              fontSize={["2.8rem", "2.8rem", "3.5rem", "5rem", "6rem"]}
-              fontFamily="Brush Script MT"
-            >
-              Diversity
-            </Text>
-          </Box>
-          <Box>
-            <Link href="/album/diversity">
-              <a>
-                <Button>View Album</Button>
-              </a>
-            </Link>
-          </Box>
-        </VStack>
-      </Flex>
-    </Box>
-  );
+	return (
+		<Box
+			w="100vw"
+			h="auto"
+			pt={["4.6rem", "6.2rem"]}
+			pb={["1.2rem", "0rem"]}
+			bg="linear-gradient(180deg, #C2B552 0%, #795806 100%)"
+			align="center"
+		>
+			<Flex direction={["column", "row"]} w={["100%", "96%", "90%", "80%"]}>
+				<CustomImage
+					src="/images/ebo-transparent.png"
+					alt="This is the main hero image of Ebo Krdums website"
+					layout="fill"
+					width="100%"
+					objectFit="contain"
+				/>
+				<VStack
+					//direction="column"
+					justify={["flex-start", "flex-end"]}
+					spacing={-1}
+					align="center"
+					my="5%"
+					//border="1px blue solid"
+				>
+					<Box>
+						<Text color="brown" fontWeight="bold" fontStyle="italic">
+							Latest Album
+						</Text>
+					</Box>
+					<Box>
+						<Text
+							color="brown"
+							fontWeight="bold"
+							fontStyle="italic"
+							fontSize={["2.8rem", "2.8rem", "3.5rem", "5rem", "6rem"]}
+							fontFamily="Alex Brush, cursive"
+						>
+							Diversity
+						</Text>
+					</Box>
+					<Box>
+						<Link href="/album/diversity">
+							<a>
+								<Button>View Album</Button>
+							</a>
+						</Link>
+					</Box>
+				</VStack>
+			</Flex>
+		</Box>
+	);
 };
 
 export default Hero;
