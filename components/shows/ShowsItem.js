f5e57e92750b01c91d0f48f7626b7fa1fc913e2b
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-09-12 11:11:42 +0200

    remove toUpperCase string method from ShowsItem due to error on Safari

--- components/shows/ShowsItem.js
+++ components/shows/ShowsItem.js
@@ -1,82 +1,82 @@
 import {
-	Box,
-	Divider,
-	Flex,
-	Link,
-	Text,
-	useMediaQuery,
-	VStack,
+  Box,
+  Divider,
+  Flex,
+  Link,
+  Text,
+  useMediaQuery,
+  VStack,
 } from "@chakra-ui/react";
 import React from "react";
 import ButtonUi from "../UI/Button";
 
 const ShowsItem = (props) => {
-	const [isLargerThan800] = useMediaQuery("(min-width:800px)");
-	const { showInfo, date, isUpcoming } = props;
-	var dayNames = [
-		"Sunday",
-		"Monday",
-		"Tuesday",
-		"Wednesday",
-		"Thursday",
-		"Friday",
-		"Saturday",
-	];
-	const monthNames = [
-		"Jan",
-		"Feb",
-		"Mar",
-		"Apr",
-		"May",
-		"Jun",
-		"Jul",
-		"Aug",
-		"Sep",
-		"Oct",
-		"Nov",
-		"Dec",
-	];
-	const month = monthNames[date.getMonth()];
-	const year = date.getFullYear();
-	const dayNr = date.getDate();
-	const dayName = dayNames[date.getDay()];
+  const [isLargerThan800] = useMediaQuery("(min-width:800px)");
+  const { showInfo, date, isUpcoming } = props;
+  var dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const monthNames = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+  const dayNr = date.getDate();
+  const dayName = dayNames[date.getDay()];
 
-	return (
-		<Box py="1rem" w="100vw" px={["5%", "8%", "10%", "20%"]}>
-			<Divider mb="2rem" />
-			<Flex direction="row" justify="space-between" align="center">
-				<VStack w="20%" align="flex-start" spacing="-0.4rem">
-					<Text fontSize={["md", "lg", "xl", "2xl"]} letterSpacing="wider">
-						{month.toUpperCase()}
-					</Text>
-					<Text fontSize={["2xl", "3xl"]} fontWeight="bold">
-						{dayNr}
-					</Text>
-					<Text fontSize={["sm", "md", "lg", "xl"]}>{year}</Text>
-				</VStack>
-				<Box w="60%" align="center">
-					<Text fontSize={["sm", "md", "lg", "xl"]}>
-						{isUpcoming ? dayName + " at " + showInfo.time : dayName}
-					</Text>
-					<Text fontSize={["sm", "md", "lg", "xl"]} fontWeight="bold">
-						{showInfo.sponsor}
-					</Text>
-					<Text fontSize={["xs", "sm", "md", "lg"]} noOfLines={2} isTruncated>
-						{showInfo.location}
-					</Text>
-				</Box>
-				<Link href={showInfo.link} w="20%" align="end" isExternal>
-					{!isLargerThan800 ? (
-						<ButtonUi>&#8594;</ButtonUi>
-					) : isUpcoming ? (
-						<ButtonUi>Buy Ticket</ButtonUi>
-					) : (
-						<a>Discover</a>
-					)}
-				</Link>
-			</Flex>
-		</Box>
-	);
+  return (
+    <Box py="1rem" w="100vw" px={["5%", "8%", "10%", "20%"]}>
+      <Divider mb="2rem" />
+      <Flex direction="row" justify="space-between" align="center">
+        <VStack w="20%" align="flex-start" spacing="-0.4rem">
+          <Text fontSize={["md", "lg", "xl", "2xl"]} letterSpacing="wider">
+            {month}
+          </Text>
+          <Text fontSize={["2xl", "3xl"]} fontWeight="bold">
+            {dayNr}
+          </Text>
+          <Text fontSize={["sm", "md", "lg", "xl"]}>{year}</Text>
+        </VStack>
+        <Box w="60%" align="center">
+          <Text fontSize={["sm", "md", "lg", "xl"]}>
+            {isUpcoming ? dayName + " at " + showInfo.time : dayName}
+          </Text>
+          <Text fontSize={["sm", "md", "lg", "xl"]} fontWeight="bold">
+            {showInfo.sponsor}
+          </Text>
+          <Text fontSize={["xs", "sm", "md", "lg"]} noOfLines={2} isTruncated>
+            {showInfo.location}
+          </Text>
+        </Box>
+        <Link href={showInfo.link} w="20%" align="end" isExternal>
+          {!isLargerThan800 ? (
+            <ButtonUi>&#8594;</ButtonUi>
+          ) : isUpcoming ? (
+            <ButtonUi>Buy Ticket</ButtonUi>
+          ) : (
+            <a>Discover</a>
+          )}
+        </Link>
+      </Flex>
+    </Box>
+  );
 };
 
 export default ShowsItem;
