b4b7fa84d604ef22e02a708879fb17b0b1761f4f
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2021-12-25 15:04:05 +0100

    fix review so user knows which language it is written on

--- components/Reviews.js
+++ components/Reviews.js
@@ -5,62 +5,63 @@ import data from "../public/locale/en/reviews";
 import ReviewCard from "./UI/ReviewCard";
 
 const Reviews = () => {
-	// const arrowStyles = {
-	//   position: "absolute",
-	//   transform: "translate(-50%, -50%)",
-	//   top: "50%",
-	//   left: "10px",
-	//   bottom: "auto",
-	//   marginTop: "-13px",
-	//   zIndex: 2,
-	// };
-	return (
-		<Box
-			w={[
-				"90vw",
-				"80vw",
-				"80vw",
-				"80vw",
-				"50vw",
-			]} /* fix layout foe the next show and video and reviews  */
-		>
-			<Carousel
-				width="100%"
-				showThumbs={false}
-				infiniteLoop
-				autoPlay
-				showStatus={false}
-				showArrows={false}
-				useKeyboardArrows
-				transitionTime={800}
-				// renderArrowPrev={(onClickHandler, hasPrev, label) => (
-				//   <button
-				//     type="button"
-				//     onClick={onClickHandler}
-				//     title={label}
-				//     style={{
-				//       ...arrowStyles,
-				//       right: "4.3em",
-				//     }}
-				//   >
-				//     <IoMdArrowDropleft fontSize="30px" />
-				//   </button>
-				// )}
-			>
-				{data.map((review) => (
-					<Link key={review.name} href={review.link} isExternal>
-						<div>
-							<ReviewCard
-								name={review.name}
-								text={review.text}
-								logo={review.logo}
-							/>
-						</div>
-					</Link>
-				))}
-			</Carousel>
-		</Box>
-	);
+  // const arrowStyles = {
+  //   position: "absolute",
+  //   transform: "translate(-50%, -50%)",
+  //   top: "50%",
+  //   left: "10px",
+  //   bottom: "auto",
+  //   marginTop: "-13px",
+  //   zIndex: 2,
+  // };
+  return (
+    <Box
+      w={[
+        "90vw",
+        "80vw",
+        "80vw",
+        "80vw",
+        "50vw",
+      ]} /* fix layout foe the next show and video and reviews  */
+    >
+      <Carousel
+        width="100%"
+        showThumbs={false}
+        infiniteLoop
+        autoPlay
+        showStatus={false}
+        showArrows={false}
+        useKeyboardArrows
+        transitionTime={800}
+        // renderArrowPrev={(onClickHandler, hasPrev, label) => (
+        //   <button
+        //     type="button"
+        //     onClick={onClickHandler}
+        //     title={label}
+        //     style={{
+        //       ...arrowStyles,
+        //       right: "4.3em",
+        //     }}
+        //   >
+        //     <IoMdArrowDropleft fontSize="30px" />
+        //   </button>
+        // )}
+      >
+        {data.map((review) => (
+          <Link key={review.name} href={review.link} isExternal>
+            <div>
+              <ReviewCard
+                name={review.name}
+                text={review.text}
+                logo={review.logo}
+                language={review.language}
+              />
+            </div>
+          </Link>
+        ))}
+      </Carousel>
+    </Box>
+  );
 };
 
 export default Reviews;
--- components/UI/ReviewCard.js
+++ components/UI/ReviewCard.js
@@ -2,55 +2,48 @@ import { Box, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
 
-const ReviewCard = ({ logo, text, name }) => {
-	return (
-		<Box
-			bg="brown"
-			shadow="md"
-			align="center"
-			maxW="3xl"
-			h={["490px", "460px", "460px", "460px", "460px"]}
-			mx="auto"
-			p={{
-				base: "6",
-				md: "8",
-			}}
-		>
-			<Box
-				w="100%"
+const ReviewCard = ({ logo, text, name, language }) => {
+  return (
+    <Box
+      bg="brown"
+      shadow="md"
+      align="center"
+      maxW="3xl"
+      h={["490px", "460px", "460px", "460px", "460px"]}
+      mx="auto"
+      p={{
+        base: "6",
+        md: "8",
+      }}
+    >
+      <Box w="100%">
+        <Image
+          src={"/images/review/" + logo}
+          alt="Ebo reviews and testimonial"
+          width={100}
+          height={100}
+          objectFit="contain"
+        />
+      </Box>
 
-				//border="solid 2px blue"
-			>
-				<Image
-					src={"/images/review/" + logo}
-					alt="Ebo reviews and testimonial"
-					width={100}
-					height={100}
-					objectFit="contain"
-				/>
-			</Box>
-
-			{/* description box */}
-			<Box
-				//border="solid 2px black"
-				h="auto"
-			>
-				<Text
-					fontSize={{ base: "sm", sm: "md", md: "lg", lg: "xl" }}
-					fontWeight="medium"
-				>
-					<ImQuotesLeft />
-					{text}
-					<ImQuotesRight />
-				</Text>
-			</Box>
-			<Box pt={["1rem"]}>
-				<Text as="cite" fontStyle="normal" fontWeight="bold">
-					{name}
-				</Text>
-			</Box>
-		</Box>
-	);
+      {/* description box */}
+      <Box h="auto">
+        <Text
+          fontSize={{ base: "sm", sm: "md", md: "lg", lg: "xl" }}
+          fontWeight="medium"
+        >
+          <ImQuotesLeft />
+          {text}
+          <ImQuotesRight />
+        </Text>
+      </Box>
+      <Box pt={["1rem"]}>
+        <Text as="cite" fontStyle="normal" fontWeight="bold">
+          Review by {name}, written in {language}
+        </Text>
+      </Box>
+    </Box>
+  );
 };
 
 export default ReviewCard;
