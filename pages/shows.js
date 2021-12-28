27e44e63f39b87083169729c83dfedc382f7eb2f
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2021-12-28 15:50:26 +0100

    update the comment

--- pages/shows.js
+++ pages/shows.js
@@ -4,34 +4,34 @@ import Section from "../components/UI/Section";
 import { shows } from "../public/locale/en/shows";
 
 const Shows = () => {
-  const todaysDate = new Date();
+	const todaysDate = new Date();
 
-  // Prepare upcomingshows from data and reverse array for latest first
-  const upcomingShows = shows.filter(
-    (show) => new Date(show.date) > todaysDate
-  );
+	// Prepare upcomingShows from data and reverse array for latest first
+	const upcomingShows = shows.filter(
+		(show) => new Date(show.date) > todaysDate
+	);
 
-  // Prepare former shows from data and reverse array
-  const formerShows = shows
-    .filter((show) => new Date(show.date) < todaysDate)
-    .reverse();
+	// Prepare former shows from data and reverse array
+	const formerShows = shows
+		.filter((show) => new Date(show.date) < todaysDate)
+		.reverse();
 
-  return (
-    <Box>
-      {/* upcoming shows*/}
-      <Section title="Upcoming Shows" pt="6rem" pb="2rem" bg="brown">
-        <Box>
-          <ShowsList shows={upcomingShows} />
-        </Box>
-      </Section>
-      {/* Former shows */}
-      <Section title="Former Shows" pt="2rem" pb="2rem" bg="yellow">
-        <Box>
-          <ShowsList shows={formerShows} isFormerShows={true} />
-        </Box>
-      </Section>
-    </Box>
-  );
+	return (
+		<Box>
+			{/* upcoming shows*/}
+			<Section title="Upcoming Shows" pt="6rem" pb="2rem" bg="brown">
+				<Box>
+					<ShowsList shows={upcomingShows} />
+				</Box>
+			</Section>
+			{/* Former shows */}
+			<Section title="Former Shows" pt="2rem" pb="2rem" bg="yellow">
+				<Box>
+					<ShowsList shows={formerShows} isFormerShows={true} />
+				</Box>
+			</Section>
+		</Box>
+	);
 };
 
 export default Shows;
