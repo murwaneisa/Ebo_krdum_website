089bfa96478437ab5ca166d56cacd1feeaed5567
Ali El Tom <23627669+eltomali@users.noreply.github.com>
2022-01-01 13:42:40 +0100

    order biography sections

--- pages/biography.js
+++ pages/biography.js
@@ -1,45 +1,40 @@
 import { Box } from "@chakra-ui/react";
-import Beginning from "../components/bio/Beginning";
 import RenderedText from "../components/bio/RenderdText";
-import Last from "../components/bio/Last";
-import MusicIdeology from "../components/bio/MusicIdeology";
 import Section from "../components/UI/Section";
 import { blockContentConverter } from "../lib/blockContentConverter";
 import sanityClient from "../lib/sanityClient";
-import { useState } from "react";
-import { Fragment } from "react/cjs/react.production.min";
 
 const Biography = ({ bios }) => {
-	return (
-		<Box>
-			{bios.map((bio, i) => {
-				return (
-					<Section
-						key={bio._id}
-						title={bio.bioSectionTitle}
-						pt={i == 0 ? "6rem" : "2rem"}
-						pb="2rem"
-						bg={i % 2 == 0 ? "brown" : "yellow"}
-					>
-						<RenderedText text={blockContentConverter(bio.bioSectionText)} />
-					</Section>
-				);
-			})}
-		</Box>
-	);
+  return (
+    <Box>
+      {bios.map((bio, i) => {
+        return (
+          <Section
+            key={bio._id}
+            title={bio.bioSectionTitle}
+            pt={i == 0 ? "6rem" : "2rem"}
+            pb="2rem"
+            bg={i % 2 == 0 ? "brown" : "yellow"}
+          >
+            <RenderedText text={blockContentConverter(bio.bioSectionText)} />
+          </Section>
+        );
+      })}
+    </Box>
+  );
 };
 
 export default Biography;
 
 export async function getStaticProps() {
-	const res = await sanityClient.fetch(`
-  *[_type == "biography"]
+  const res = await sanityClient.fetch(`
+  *[_type == "biography"] | order(order asc)
   `);
-	return {
-		props: {
-			bios: res,
-		},
-	};
+  return {
+    props: {
+      bios: res,
+    },
+  };
 }
 
 /* 	
