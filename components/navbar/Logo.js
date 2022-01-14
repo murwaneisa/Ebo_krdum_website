c20b4af0da3be2b3b17194cae49a9b4e81e2c034
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-01-14 15:31:53 +0100

    update the navbar

---
+++ components/navbar/Logo.js
@@ -0,0 +1,24 @@
+import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Logo(props) {
+	return (
+		//  <Box {...props}>
+
+		<Box pl={["0", "4", "10"]}>
+			<Link href="/">
+				<a>
+					<Image
+						src="/images/logo_white.png"
+						alt="Ebo Krdum logo"
+						height="20"
+						width="100"
+					/>
+				</a>
+			</Link>
+		</Box>
+
+		// </Box>
+	);
+}
--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,43 +1,25 @@
-import { Box, HStack, Stack } from "@chakra-ui/react";
+import { Box, HStack, Stack, Flex, IconButton } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import Image from "next/image";
 import Link from "next/link";
 import MenuItem from "./MenuItem";
+import { useState } from "react";
+import NavbarContainer from "./navbarContainer";
+import Logo from "./Logo";
+import MenuToggle from "./menuToggle";
+import MenuItemContainer from "./menuItemContainer";
 
 const Nav = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
+
 	return (
-		<Stack
-			position="absolute"
-			zIndex={3}
-			direction={["column", "row"]}
-			justify={["center", "space-between"]}
-			align="center"
-			m="0 auto"
-			mb="20px"
-			minWidth="100vw"
-			p={["4", "6", "8"]}
-		>
-			<Box pl={["0", "4", "10"]}>
-				<Link href="/">
-					<a>
-						<Image
-							src="/images/logo_white.png"
-							alt="Ebo Krdum logo"
-							height="20"
-							width="100"
-						/>
-					</a>
-				</Link>
-			</Box>
-			<HStack pr={["0", "4", "10"]} spacing={["4", "8", "12"]}>
-				<MenuItem to="/">Home</MenuItem>
-				<MenuItem to="/albums">Shows</MenuItem>
-				<MenuItem to="/shows">Shows</MenuItem>
-				<MenuItem to="/Gallery">Gallery</MenuItem>
-				<MenuItem to="/biography">Biography</MenuItem>
-				<MenuItem to="/blog">blog</MenuItem>
-				<MenuItem to="/contacts">Contacts</MenuItem>
-			</HStack>
-		</Stack>
+		<NavbarContainer>
+			<Logo />
+			<MenuToggle toggle={toggle} isOpen={isOpen} />
+			<MenuItemContainer isOpen={isOpen} />
+		</NavbarContainer>
 	);
 };
 
