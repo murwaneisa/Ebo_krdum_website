10833ee744318eb8882e753e153c11aec303de67
murwaneisa <59895485+murwaneisa@users.noreply.github.com>
2022-04-21 02:04:27 +0200

    updating the navbar

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,8 +1,3 @@
-import { Box, HStack, Stack, Flex, IconButton } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import Image from "next/image";
-import Link from "next/link";
-import MenuItem from "./MenuItem";
 import { useState } from "react";
 import NavbarContainer from "./navbarContainer";
 import Logo from "./Logo";
--- components/navbar/menuItemContainer.js
+++ components/navbar/menuItemContainer.js
@@ -1,26 +1,47 @@
-import { Box, Stack } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Collapse,
+	Stack,
+	Link,
+	useColorModeValue,
+} from "@chakra-ui/react";
 import React from "react";
 import MenuItem from "./MenuItem";
 
 const MenuItemContainer = ({ isOpen }) => {
+	const [show, setShow] = React.useState(false);
+	const handleToggle = () => setShow(!show);
 	return (
 		<Box
 			display={{ base: isOpen ? "block" : "none", md: "block" }}
 			flexBasis={{ base: "100%", md: "auto" }}
 		>
 			<Stack
-				spacing={[8, 2, 8]}
+				spacing={[4, 2, 8]}
 				align="center"
 				justify={["center", "space-between", "flex-end", "flex-end"]}
 				direction={["column", "row", "row", "row"]}
 				pt={[4, 4, 0, 0]}
 			>
-				<MenuItem to="/albums">Albums</MenuItem>
+				<MenuItem to="/albums">Home</MenuItem>
 				<MenuItem to="/shows">Shows</MenuItem>
-				<MenuItem to="/Gallery">Gallery</MenuItem>
+				<Link onClick={handleToggle}>Press</Link>
+				<Collapse in={show} animateOpacity style={{ marginTop: "0!important" }}>
+					<Stack
+						mt={2}
+						pl={4}
+						borderLeft={1}
+						borderStyle={"solid"}
+						borderColor={useColorModeValue("gray.200", "gray.700")}
+						align={"start"}
+					>
+						<Link>Press</Link>
+						<Link>Press</Link>
+						<Link>Press</Link>
+					</Stack>
+				</Collapse>
 				<MenuItem to="/biography">Biography</MenuItem>
-				<MenuItem to="/blog">blog</MenuItem>
-				<MenuItem to="/contacts">Contacts</MenuItem>
 			</Stack>
 		</Box>
 	);
